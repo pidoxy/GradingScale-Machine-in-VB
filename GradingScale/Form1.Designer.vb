88bb625d6ddc75e2dcd9a46e9539a5c55be72832
IDOKO EMMANUEL <pidox31102002@gmail.com>
2022-11-04 18:39:25 +0100

    Add project files.

---
+++ GradingScale/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class GradingScale
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ScoreBox = New System.Windows.Forms.TextBox()
+        Me.GradeBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ScoreBox
+        '
+        Me.ScoreBox.Location = New System.Drawing.Point(273, 98)
+        Me.ScoreBox.Name = "ScoreBox"
+        Me.ScoreBox.Size = New System.Drawing.Size(169, 20)
+        Me.ScoreBox.TabIndex = 0
+        '
+        'GradeBox
+        '
+        Me.GradeBox.Location = New System.Drawing.Point(273, 228)
+        Me.GradeBox.Name = "GradeBox"
+        Me.GradeBox.Size = New System.Drawing.Size(169, 20)
+        Me.GradeBox.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(318, 182)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Padding = New System.Windows.Forms.Padding(4)
+        Me.Label1.Size = New System.Drawing.Size(80, 34)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Grade"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(321, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Padding = New System.Windows.Forms.Padding(4)
+        Me.Label2.Size = New System.Drawing.Size(77, 34)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Score"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(294, 329)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Padding = New System.Windows.Forms.Padding(4)
+        Me.Button1.Size = New System.Drawing.Size(135, 41)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Get Grade"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GradingScale
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GradeBox)
+        Me.Controls.Add(Me.ScoreBox)
+        Me.Name = "GradingScale"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ScoreBox As TextBox
+    Friend WithEvents GradeBox As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ GradingScale/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class GradingScale
+    Dim Score As Double
+    Dim Grade
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Score = Val(ScoreBox.Text)
+
+        If Score = 0 Then
+            MsgBox("Put a value in the score box")
+        Else
+            Select Case Score
+                Case 0 To 39
+                    Grade = "F"
+                Case 40 To 44
+                    Grade = "E"
+                Case 45 To 49
+                    Grade = "D"
+                Case 50 To 59
+                    Grade = "C"
+                Case 60 To 69
+                    Grade = "B"
+                Case 70 To 100
+                    Grade = "A"
+            End Select
+        End If
+
+        GradeBox.Text = Grade
+        MsgBox(Grade)
+
+    End Sub
+End Class
